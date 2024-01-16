9fff0049a758fe2b863beaa09d7882f125295fcd
ajitkumar7432 <156606144+ajitkumar7432@users.noreply.github.com>
2024-01-17 01:29:16 +0530

    Add files via upload

---
+++ Day2/PS_2_5.java
@@ -0,0 +1,7 @@
+public class PS_2_5 {
+    public static void main(String[] args) {
+        int x = 7;
+        int a= 7 * 49 / 7 + 35 / 7 ;
+        System.out.println("The value of a is : " + a);
+    }
+}

2b78e4d059e349aad4e527acfe684dadbcc46661
ajitkumar7432 <156606144+ajitkumar7432@users.noreply.github.com>
2024-02-17 22:56:17 +0530

    Delete Day15 directory

--- Day15/PS_15_2.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Date;
-
-public class PS_15_2 {
-   public static void main(String[] args) {
-    Date d = new Date();
-    System.out.println(d.getHours() + ":" + d.getMinutes()+":" +d.getSeconds());  // prints the current hour and minute
-   } 
-}
